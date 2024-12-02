77296a57c99796b35b6562dca20c01b8439e8a8e
chikaama <jantachika@gmail.com>
2024-12-02 20:19:10 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+//array of quiz questions and their corresponding answers
+const questions = [
+  {
+    question: "What does HTML stand for?", 
+    options: [ 
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyperlinks Text Markup Language",
+      "High-Level Machine Language"
+    ],
+    correct: 0
+  },
+  {
+    question: "Which language is used for styling web pages?",
+    options: ["HTML", "CSS", "JavaScript", "Python"],
+    correct: 1
+  },
+  {
+    question: "Which is not a programming language?",
+    options: ["Python", "JavaScript", "HTML", "C++"],
+    correct: 2
+  },
+  {
+    question: "Which HTML tag is used to define an internal stylesheet?",
+    options: ["<style>", "<script>", "<css>", "<link>"],
+    correct: 0
+  },
+  {
+    question: "Which symbol is used to select an ID in CSS?",
+    options: [".", "#", "!", "$"],
+    correct: 1
+  },
+  {
+    question: "Which company developed JavaScript?",
+    options: ["Microsoft", "Apple", "Netscape", "Google"],
+    correct: 2
+  },
